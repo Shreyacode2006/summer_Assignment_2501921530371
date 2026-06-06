e303d1bc468c8fb6f9ad2f579bd654eb100ce96d
Shreyacode2006 <25113cn248@glbitm.ac.in>
2026-06-06 10:58:14 +0530

    Add files via upload

---
+++ Day6/program24.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{ int x, n;
+    long long result = 1;
+
+    printf("enter base (x): ");
+    scanf("%d", &x);
+
+    printf("enter power (n): ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        result *= x;
+    }
+
+    printf("%d * %d = %lld", x , n , result);
+
+    return 0;
+
+}
